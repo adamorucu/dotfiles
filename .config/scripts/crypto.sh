1212f09c7bd72f1de75395a5edb168cc0d375721
adamorucu <adamdorucu@gmail.com>
2021-11-26 21:27:45 +0100

    New scripts

--- .config/scripts/crypto.sh
+++ .config/scripts/crypto.sh
@@ -1,18 +1,26 @@
-symbols=$'BTC\nETH\nADA\nDOT\nXRP'
+symbols=$'BTC\nETH\nSOL\nDOT\nADA\nXRP'
 
 echo "$symbols" | while read coin
 do
-    price=$(curl -s "rate.sx/$coin?qFT" | grep "change.*(.*)" | awk -F '[()]' '{print $2}')
+    # change=$(curl -s "rate.sx/$coin?qFT" | grep "change.*(.*)" | awk -F '[()]' '{print $2}')
+    price=$(curl -s "rate.sx/$coin?qFT" | grep -P "avg.*?//" | awk '{print $2}'| sed 's/\$//' )
+    price=$( echo "$price" | sed 's/\./,/' )
+    price=$( printf "%.2f" "$price" )
+    # rounded=$( printf "%.1f" "$price" )
     if [ $coin = "BTC" ]; then
         icon=""
+        price=$( echo "$price" | awk -F',' '{print $1}' )
     elif [ $coin = "ETH" ]; then
         icon=""
+        price=$( echo "$price" | awk -F',' '{print $1}' )
     elif [ $coin = "ADA" ]; then
         icon=""
     elif [ $coin = "XRP" ]; then
         icon=""
     elif [ $coin = "DOT" ]; then
         icon=""
+    elif [ $coin = "SOL" ]; then
+        icon=""
     fi
     echo -n "$icon $price  "
 done
